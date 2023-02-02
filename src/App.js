fb072595371820992c689cd4145f9c4fc0fcc136
JD Tadlock <jt7903@gmail.com>
2023-02-01 20:35:11 -0500

    toggle example

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
 
+
 function App() {
   const title = 'Learning React - All the pieces!';
+  const [showMain, setShowMain] = useState(true); // [0, function() {}]
 
   return (
     <div>
       <Header title={title} />
 
-      <Main />
+      {showMain && <Main />}
+
+      <button onClick={() => setShowMain(!showMain)}>{showMain ? 'Hide' : 'Show'} Main</button>
 
       <Footer title={title} />
     </div>
